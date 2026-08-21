bb05a1b86160fe082f2baaabbc8793e2ab2c3470
digitalmapdev <kontakt@digitalmap.pl>
2026-08-21 20:56:51 +0200

    Create supabase.js

---
+++ assets/js/supabase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL = "https://xnotuefxovsctkgutpbn.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_Y9v3V_3C_KspOttshO5BYQ_Eh11rnYl";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
+console.log("Supabase connected", supabaseClient);
